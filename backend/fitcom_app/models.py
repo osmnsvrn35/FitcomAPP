f25881d59b20cc5ce85aeabed0d5ca6f82dff93e
osmnsvrn35 <osmnsvrn35@gmail.com>
2024-11-18 20:23:18 +0100

    feat: creation of exercise table

--- backend/fitcom_app/models.py
+++ backend/fitcom_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Level(models.TextChoices):
+    BEGINNER = 'beginner', ('Beginner')
+    INTERMEDIATE = 'intermediate', ('Intermediate')
+    EXPERT = 'expert', ('Expert')
+
+class Exercise(models.Model):
+    exercise_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=200)
+    body_part = models.CharField(max_length=100)
+    equipment = models.CharField(max_length=100)
+    gif_url = models.URLField()
+    target = models.CharField(max_length=100)
+    secondary_muscles = models.JSONField(default=list)
+    instructions = models.JSONField(default=list)
+    level = models.CharField(
+        max_length=20,
+        choices=Level.choices,
+        default=Level.BEGINNER
+    )
--- backend/fitcom_project/settings.py
+++ backend/fitcom_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fitcom_project',
+    'fitcom_app',
 ]
 
 MIDDLEWARE = [
