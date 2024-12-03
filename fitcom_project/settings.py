0fcae5d650fed9559d7e995cdc824282bcddbed7
osmnsvrn35 <osmnsvrn35@gmail.com>
2024-12-03 15:07:21 +0100

    fix: attempt to make the request

--- fitcom_project/settings.py
+++ fitcom_project/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_HOSTS = [
     "localhost",
     'http://192.168.0.159:8081',
     "fitcom-9fc3ecf39e06.herokuapp.com/api/users/",
+    "http://localhost:8081",
 
 ]
 CORS_ALLOW_HEADERS = [ "accept", "referer", "accept-encoding", "authorization", "content-type", "dnt", "origin", "user-agent", "x-csrftoken", "x-requested-with" ]
@@ -97,7 +98,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
